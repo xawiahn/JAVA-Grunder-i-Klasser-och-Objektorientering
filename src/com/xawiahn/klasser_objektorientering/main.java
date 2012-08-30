9e05b7d8c488498311e394009f29f08456201f9a
Xawiahn <xawiahn@gmail.com>
2012-08-30 13:49:06 +0200

    övning 1 och 2

--- src/com/xawiahn/klasser_objektorientering/main.java
+++ src/com/xawiahn/klasser_objektorientering/main.java
@@ -1,27 +1,61 @@
 package com.xawiahn.klasser_objektorientering;
+import java.util.Scanner;
 
 public class main {
-class Elev
-{
-	int provPoang;
-	String elevNamn;
 	
-	Elev(int pP, String eNamn)
+	/* INTRO "UPPGIFTER"
+	public static void main(String args[])
 	{
-		provPoang = pP;
-		elevNamn = eNamn;
+		class Elev
+		{
+			int provPoang;
+			String elevNamn;
+			void skrivut()
+			{
+				System.out.println("Namn: " + elevNamn + " Poäng: " + provPoang);
+			}
+			Elev(int pP, String eNamn)
+			{
+				provPoang = pP;
+				elevNamn = eNamn;
+			}
+		}
+		Elev student = new Elev(14, "Kalle Anka");
+		student.skrivut();
+		student.provPoang = 16;
+		student.skrivut();
 	}
-	void skrivut()
-	{
-		System.out.println("Namn: " + elevNamn + " Poäng: " + provPoang);
-	}
+}*/
 
-}
-public static void main(String args[])
-{
-	Elev student = new Elev(14, "Kalle Anka");
-	student.skrivut();
-	student.provPoang = 16;
-	student.skrivut();
-}
+	/* ÖVNING 1
+	public static void main(String[] args) {
+		
+		int tal = 2;
+		Scanner sc = new Scanner(System.in);
+		while(tal <= 20)
+		{
+			System.out.println(tal);
+			tal += 2;
+		}
+	}
+}*/
+	
+	/* ÖVNING 2
+	public static void main(String[] args) {
+		
+		int tal[] = new int[3], raknare = 0, summa;
+		tal[0] = 1;
+		tal[1] = 2;
+		tal[2] = 3;
+		Scanner sc = new Scanner(System.in);
+		while(raknare <20)
+		{
+			summa = tal[0] + tal[1] + tal[2];
+			System.out.println(summa);
+			raknare += 1;
+			tal[0] = tal[1];
+			tal[1] = tal[2];
+			tal[2] = summa;
+		}
+	}*/
 }
